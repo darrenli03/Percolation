e345a04c5f46df5fb607bbeea74a9a067aa7aae8
kevin.han@duke.edu <kxh0423@gmail.com>
2023-11-12 21:59:16 -0500

    fefefefe

--- src/PercolationUF.java
+++ src/PercolationUF.java
@@ -8,10 +8,48 @@ public class PercolationUF implements IPercolate {
     public PercolationUF(IUnionFind finder, int size)
     {
         myGrid = new boolean[size][size];
-        myFinder.initialize(size * size + 2);
+        finder.initialize(size * size + 2);
+        myFinder = finder;
         VTOP = size * size;
         VBOTTOM = size * size + 1;
         myOpenCount = 0;
+    }
+
+    @Override
+    public boolean isOpen(int row, int col) {
+		
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException(
+					String.format("(%d,%d) not in bounds", row,col));
+		}
+		return myGrid[row][col] != BLOCKED;
+	}
+
+    @Override
+    public boolean isFull(int row, int col) {
+		
+		if (! inBounds(row,col)) {
+			throw new IndexOutOfBoundsException(
+					String.format("(%d,%d) not in bounds", row,col));
+		}
+		
+		return myGrid[row][col] == FULL;
+	}
+
+    @Override
+    public boolean percolates()
+    {
+        return connected(VTOP, VBOTTOM);
+    }
+
+    @Override
+    public int numberOfOpenSites(){
+        return myOpenCount;
+    }
+
+    @Override
+    public void open(int row, int col){
+
     }
 
     
